4997a5f580696dc222c8468fd6ba4780afd30382
shelepuginivan <shelepuginivanm@gmail.com>
2025-01-04 07:18:38 +0300

    fix(preview): ignore thumbnails to avoid duplicates

--- .github/scripts/preview.sh
+++ .github/scripts/preview.sh
@@ -4,6 +4,13 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+case "$1" in
+    ".github/thumbnails/"* | "./.github/thumbnails"*)
+        echo "Ignoring thumbnail..."
+        exit 1
+        ;;
+esac
+
 PREVIEW="PREVIEW.md"
 BASENAME="$(basename "$1")"
 THUMBNAIL=".github/thumbnails/$BASENAME"
